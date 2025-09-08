91b45b5f7482247844dfbc5c4805e1d98855c9f3
Franquex <franquitoturey29@gmail.com>
2025-09-08 15:18:39 -0600

    Subiendo proyecto

--- app/src/main/java/com/franquex/infogram/CreateAccountActivity.java
+++ app/src/main/java/com/franquex/infogram/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.franquex.infogram;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,21 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getString(R.string.tittle_crear_cuenta), true);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void showToolbar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
 }
